b6e6d40779d9e76f115bae0b2293eba83dccc163
Maleesha Fonseka <maleesha.fonseka@outlook.com>
2026-01-01 09:47:43 +0530

    Updated App.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,4 +1,4 @@
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
+import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom'
 import './App.css'
 import Home from './pages/Home/Home.jsx'
 import Login from './pages/Login/Login.jsx'
@@ -9,6 +9,7 @@ function App() {
   return (
   <Router>
     <Routes>
+      <Route path="/" element={<Navigate to="/home" replace />} />
       <Route path="/home" exact element={<Home />} />
       <Route path="/login" exact element={<Login />} />
       <Route path="/signup" exact element={<SignUp />} />
